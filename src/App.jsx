16f6ea851d76e80dbfe79ca67060f604141b2b02
Chester Bernabe <bernabealan05@gmail.com>
2024-04-21 15:50:06 +0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { Router, Link, Navigate, Route, Routes } from 'react-router-dom';
 import {ListSongs} from "./components/ListSongs";
+import { Player } from './components/Player';
 function App() {
 
   return (
     <>
-      <ListSongs />
+      <Routes>
+        <Route path="/" element={<ListSongs/>}/>
+        <Route path="/player" element={<Player/>}/>
+      </Routes>
     </>
   )
 }
